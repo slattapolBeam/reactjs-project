77ee30c83716c9021f9b30f023e752bce5e4c0f5
= <=>
2026-02-18 21:36:05 +0700

    edit loading

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ interface Product {
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<any>(null);
   const [view, setView] = useState<"inventory" | "logs" | "dashboard">(
     "dashboard",
